4aedd644e79b140116048611d7c3407e9c4f93b0
Benjamin Dirgo <bdirgo@gmail.com>
2020-04-30 14:27:54 -0700

    Initial Commit

---
+++ DisapperingUnicorns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DisapperingUnicorns
+//
+//  Created by Benjamin Dirgo on 4/30/20.
+//  Copyright © 2020 Benjamin Dirgo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
